e1caf937644a3da3166d3478df3c625faf34a935
Ben <Ben@Gardner-PC>
2013-08-27 17:54:58 -0500

    First Commit

---
+++ src/inheritance1/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance1;
+
+/**
+ *
+ * @author Ben
+ */
+public interface Employee {
+    
+}
---
+++ src/inheritancepractice/Driver.java
@@ -0,0 +1,12 @@
+package inheritancepractice;
+
+
+public class Driver {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+
+}
