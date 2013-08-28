81669c78c3cada178a7052ff9dfb53031c86758a
Ben <Ben@Gardner-PC>
2013-08-27 19:36:48 -0500

    --Added implementations to the abstract classes
    --Main Driver class is not liking setting the employee variable of type Employee to a SalaryPlusBonusEmployee type

--- src/inheritancepractice/Driver.java
+++ src/inheritancepractice/Driver.java
@@ -1,5 +1,6 @@
 package inheritancepractice;
 
+import abstract1.*;
 import concrete.*;
 
 public class Driver {
@@ -7,6 +8,34 @@ public class Driver {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        /* TODO CONCRETE CLASS TESTING
+        ------------------------------*/
+        
+        /* ABSTRACT CLASS TESTING
+        --------------------------*/ 
+        
+        //Test hourly employee
+        abstract1.Employee employee;
+        employee = new abstract1.HourlyEmployee("ben", 27, "male", 20, 40);
+        
+        System.out.println("Test for hourly employee:");
+        System.out.println(employee + "\n");
+        
+        //Test Salaried employee
+        employee = new abstract1.SalariedEmployee("Jill", 35, "female", 30000);
+        
+        System.out.println("Test for salaried employee:");
+        System.out.println(employee + "\n");
+        
+        //Test Salaried plus bonus employee
+        employee = new abstract1.SalaryPlusBonusEmployee("Jane", 42, "female", 50000);
+        employee.setBonus(5000);
+        
+        
+        System.out.println("Test for salaried employee:");
+        System.out.println(employee + "\n");
+        
         
     }
 
